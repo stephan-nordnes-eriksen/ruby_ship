596426daeabe771350a2ed18b9211d7780fd1595
Stephan Nordnes Eriksen <stephanruler@gmail.com>
2014-07-20 02:28:36 +0200

    Adding critical fix to windows as well.

--- tools/ruby_ship_build.bat
+++ tools/ruby_ship_build.bat
@@ -17,25 +17,34 @@ rm -rf %~dp0/extracted_ruby
 mkdir %~dp0/extracted_ruby
 mkdir %~dp0/../bin/win_ruby
 
-:: Extracting ruby source
+:: UNZIP SOURCE
 7z x %~f1 -so | 7z x -aoa -si -ttar -o"%~dp0/extracted_ruby"
 
+:: GET RUBY VERSION AND DIRECTORY
 for /f %%i in ('ls %~dp0/extracted_ruby') do set RUBYDIR=%%i
 for /f %%i in ('echo %RUBYDIR% ^| cut -d'-' -f 2') do set RUBY_VERSION=%%i
 
-echo "Installing ruby version %RUBY_VERSION%"
+echo "############################"
+echo "Ruby Ship is Installing ruby version %RUBY_VERSION%"
+echo "############################"
 
 :: BUILDING RUBY
 cd "%~dp0/extracted_ruby/%RUBYDIR%"
-call %~dp0/extracted_ruby/%RUBYDIR%/win32/configure.bat --prefix=%~dp0/../bin/win_ruby
+call %~dp0/extracted_ruby/%RUBYDIR%/win32/configure.bat --enable-load-relative --prefix=%~dp0/../bin/shipyard/win_ruby
 nmake
 nmake test
 nmake install
 
-:: MAKING THE SCRIPT:
-for /f %%i in ('ls %~dp0/../bin/win_ruby/include') do set RUBY_INSTALL_DIR=%%i
-echo %RUBY_INSTALL_DIR%
+:: SETTING UP REFERENCE DIRECTORIES
+for /f %%i in ('ls %~dp0/../bin/shipyard/win_ruby/include') do set RUBY_INSTALL_DIR=%%i
 for /f %%i in ('echo %RUBY_INSTALL_DIR% ^| cut -d'-' -f 2') do set RUBY_VERSION_DIR=%%i
+for /f %%i in ('ls %~dp0/../bin/shipyard/win_ruby/lib/ruby/%RUBY_VERSION_DIR% | FindStr /mswin/') do set RUBY_BINARY_VERSION_DIR=%%i
+:: SETTING UP COMMON WRAPPER COMPONENTS
+
+:: MAKING THE SCRIPT:
+
+echo %RUBY_INSTALL_DIR%
+
 
 echo @echo off > %~dp0/../bin/win_ruby.bat
 echo %%~dp0\win_ruby\bin\ruby.exe %%* >> %~dp0/../bin/win_ruby.bat
