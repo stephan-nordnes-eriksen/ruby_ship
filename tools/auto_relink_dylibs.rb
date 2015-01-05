939247632f202615ab68a39e9d2742f6c8cfdca4
Stephan Nordnes Eriksen <stephanruler@gmail.com>
2015-01-05 09:31:33 +0400

    updating the dylib relinker to be recursive and link from/to correct folders

--- tools/auto_relink_dylibs.rb
+++ tools/auto_relink_dylibs.rb
@@ -1,13 +1,9 @@
 require "fileutils"
-new_dylib_path = File.join("..","bin","shipyard","darwin_ruby","ruby_ship_dylibs")
-FileUtils.mkdir_p(new_dylib_path)
+@new_dylib_path = File.join("bin", "shipyard", "darwin_ruby", "ruby_ship_dylibs")
 
-folders = ["../bin/shipyard/darwin_ruby"]
+FileUtils.mkdir_p(@new_dylib_path)
 
-full = folders.map{|f| Dir[File.join(f, '**', '*')]}
-actual_files = full.flatten(1).uniq.select{|e| File.file? e}
-
-actual_files.each do |file|
+def fix_dylib_for_file(file)
 	results = `otool -L #{file}`
 
 	if results.is_a?(String) && !results.include?("#{file}: is not an object file")
@@ -20,13 +16,14 @@ actual_files.each do |file|
 		lines[1..-1].each do |libfile_line|
 			libfile = libfile_line.split(" (compatibility version")[0].strip
 			libfile = libfile.split("(")[0]
-			next if libfile.include?(new_dylib_path)
+			next if libfile.include?(@new_dylib_path)
 			
-			new_libfile = File.join(new_dylib_path, libfile.split(File.join("",""))[-1])
+			new_libfile = File.join(@new_dylib_path, libfile.split(File.join("",""))[-1])
 	
 			unless File.file?(new_libfile)
 				FileUtils.copy(libfile, new_libfile)
 				puts "Copied #{new_libfile}"
+				fix_dylib_for_file(new_libfile)
 			end
 			relink_command_results = `sudo install_name_tool -change #{libfile} #{new_libfile} #{file} 2> /dev/null`
 
@@ -36,3 +33,17 @@ actual_files.each do |file|
 		end
 	end
 end
+
+puts File.dirname(File.expand_path($0))
+
+folders = [File.expand_path(File.join(File.dirname(File.expand_path($0)), "..", "bin", "shipyard", "darwin_ruby"))]
+
+full = folders.map{|f| Dir[File.join(f, '**', '*')]}
+actual_files = full.flatten(1).uniq.select{|e| File.file? e}
+
+
+actual_files.each do |file|
+	fix_dylib_for_file(file)
+end
+
+puts "Relinking of dylib done"
