43c7c52bd6051e761f610d8018a0305277fc2fc3
Stephan Nordnes Eriksen <stephanruler@gmail.com>
2015-01-05 09:39:24 +0400

    updating darwin wrappers

--- bin/shipyard/darwin_gem.sh
+++ bin/shipyard/darwin_gem.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-GEM_PATH=$DIR/darwin_ruby/lib/ruby/gems/2.1.0/:$DIR/darwin_ruby/lib/ruby/2.1.0/:$DIR/darwin_ruby/bin/:$DIR/darwin_ruby/lib/ruby/2.1.0/x86_64-darwin13.0/
+GEM_PATH=$DIR/darwin_ruby/lib/ruby/gems/2.1.0/:$DIR/darwin_ruby/lib/ruby/2.1.0/:$DIR/darwin_ruby/bin/:$DIR/darwin_ruby/lib/ruby/2.1.0/x86_64-darwin14.0/
 GEM_HOME=$DIR/darwin_ruby/lib/ruby/gems/2.1.0/
 $DIR/darwin_ruby/bin/gem "$@"
