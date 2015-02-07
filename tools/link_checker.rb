cb4661b68c4f8f7525ce0e942989f13b78279cef
Stephan Nordnes Eriksen <stephanruler@gmail.com>
2015-02-07 15:54:45 +0100

    Updating link_checker

--- tools/link_checker.rb
+++ tools/link_checker.rb
@@ -1,36 +1,20 @@
+#Meant for debugging only. It will traverse all Mach-O files and check if there are any invalid links. 
+#Warning: Not an exhaustive search. There can be many other issues than what this script finds
+
 require "fileutils"
 @new_dylib_path = File.join("shipyard", "darwin_ruby", "dylibs")
 
-
-
 def check_dylib(file)
-	
 	results = `otool -L #{file}` #Will get information about which dylibs to link
 	
 	if results.is_a?(String) && results != "" && !results.include?("is not an object file") && !results.include?("Assertion failed:")
 		# puts "checking #{file}"
 		if !results.split("\n").select{|e| e.include?(File.join("usr","local"))}.empty?
 			puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-			puts "2: #{file}"
+			puts "Invalid link in: #{file}"
 			puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 		end
-		return
-		results.split("\n").each_with_index{|e, i| puts "#{i}: #{e.strip}"}
-
-		#Setting new ID for this if needed
-		id_path = File.join("shipyard", file.split(File.join("shipyard"))[-1])
-		# id_reset_results = `sudo install_name_tool -id #{id_path} #{file} 2> /dev/null` #Will link the current file to itself
-		
-
-		lines = results.split("\n")
-		lines = [] unless lines
-		itterate = lines[1..-1]
-		itterate = [] unless itterate
-
-
-		itterate.each do |libfile_line|
-			puts "Something wrong with: #{file}" if libfile_line.include?("/usr/")
-		end
+		# results.split("\n").each_with_index{|e, i| puts "#{i}: #{e.strip}"}
 	end
 end
 
