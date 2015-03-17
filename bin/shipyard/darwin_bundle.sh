d63243fb7139b505c97ea0eed6a8963f50fcb79c
Stephan Nordnes Eriksen <stephanruler@gmail.com>
2015-03-17 08:44:41 +0100

    Fixing indentation in darwin_ruby

--- bin/shipyard/darwin_bundle.sh
+++ bin/shipyard/darwin_bundle.sh
@@ -2,7 +2,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GEM_PATH="${DIR}/darwin_ruby/lib/ruby/gems/2.1.0/:${DIR}/darwin_ruby/lib/ruby/2.1.0/:${DIR}/darwin_ruby/bin/:${DIR}/darwin_ruby/lib/ruby/2.1.0/x86_64-darwin14.0/"
 GEM_HOME="${DIR}/darwin_ruby/lib/ruby/gems/2.1.0/"
 "${DIR}/darwin_ruby/lib/ruby/gems/2.1.0/bin/bundle" "$@"
-if [ "$1" == "install" ] || ["$1" == "update"]; then
+if [ "$1" == "install" ] || [ "$1" == "update" ] ; then
 	cd "${DIR}/../../"
 	ruby "./tools/auto_relink_dylibs.rb"
 fi
