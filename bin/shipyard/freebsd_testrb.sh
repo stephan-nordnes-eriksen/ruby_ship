4c745353c40a5abb48f3f50d7babd0b2229a6631
olevole <olevole@olevole.ru>
2015-09-25 02:45:11 +0300

    Make ruby_ship possible on FreeBSD platform. Tested with: FreeBSD 10.2, FreeBSD 11-head and Ruby 2.2.3

---
+++ bin/shipyard/freebsd_testrb.sh
@@ -0,0 +1,4 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GEM_PATH="${DIR}/freebsd_ruby/lib/ruby/gems/2.2.0/:${DIR}/freebsd_ruby/lib/ruby/2.2.0/:${DIR}/freebsd_ruby/bin/:${DIR}/freebsd_ruby/lib/ruby/2.2.0/x86_64-freebsd11.0/"
+GEM_HOME="${DIR}/freebsd_ruby/lib/ruby/gems/2.2.0/"
+"${DIR}/freebsd_ruby/bin/testrb" "$@"
